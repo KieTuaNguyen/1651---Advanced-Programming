d1b65f31716e7f1960003b016796dc72a9326866
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-01-09 16:15:02 +0700

    EX2 submition

---
+++ ConsoleApp2/Ex2.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp2;
+
+StudentInformation firstStudent = new StudentInformation();
+Console.Write("Enter the name of that student: ");
+firstStudent.Name = Console.ReadLine();
+Console.Write("Enter the age of that student: ");
+firstStudent.Age = int.Parse(Console.ReadLine());
+Console.Write("Enter the GPA of that student: ");
+firstStudent.Gpa = double.Parse(Console.ReadLine());
+Console.WriteLine(firstStudent);
---
+++ ConsoleApp2/StudentInformation.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp2;
+
+public class StudentInformation:Object
+{
+    //Field
+    private int age;
+    private double gpa;
+    private string name;
+    
+    //Properties
+    public int Age
+    {
+        set { if(value>0) age = value;  }
+        get { return age;  }
+    }
+
+    public double Gpa
+    {
+        set { if(value>0 || value<=4) gpa = value;  }
+        get { return gpa;  }
+    }
+
+    public string Name
+    {
+        set { name = value; }
+        get { return name; }
+    }
+    
+    public void DisplayInformation()
+    {
+        Console.WriteLine($"Student named " + name +  " with age " + age + " has a GPA of " + gpa + ".");
+    }
+    
+    public override string ToString()
+    {
+        return $"Student named " + name +  " with age " + age + " has a GPA of " + gpa + ".";
+    }
+}
