49050159783d36ffe1a15526bddcbc15701279fe
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-01-11 12:50:11 +0700

    Homework: EX3 submition

---
+++ ConsoleApp3/Ex3.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp3;
+
+Song s = new Song();
+Console.WriteLine("Enter the number of songs: ");
+int numOfSongs = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the data in this format: {type list}_{name}_{time}: ");
+List<Song> songs = new List<Song>();
+Console.WriteLine("---------------------");
+for (int i = 0; i < numOfSongs; i++)
+{
+  string[] data = Console.ReadLine().Split('_');
+  Song song = new Song(data[0], data[1], data[2]);
+  songs.Add(song);
+}
+Console.WriteLine("---------------------");
+Console.WriteLine("Enter the type list: ");
+string typeList = Console.ReadLine();
+if (typeList == "all")
+{
+  foreach (Song song in songs)
+  {
+    Console.WriteLine(song.Name);
+  }
+}
+else
+{
+  Console.WriteLine("Below are the songs included in the " + typeList + ": ");
+  Console.WriteLine("---------------------");
+  foreach (Song song in songs)
+  {
+    if (song.TypeList == typeList)
+    {
+      Console.WriteLine(song.Name);
+    }
+  }
+}
---
+++ ConsoleApp3/Song.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp3
+{
+  internal class Song
+  {
+    //Field
+    private string typeList;
+    private string name;
+    private string time;
+    
+    //Properties
+    public string TypeList
+    {
+      set { typeList = value; }
+      get { return typeList; }
+    }
+    public string Name
+    {
+      set { name = value; }
+      get { return name; }
+    }
+    public string Time
+    {
+      set { time = value; }
+      get { return time; }
+    }
+
+    public Song()
+    {
+      
+    }
+    public Song(string typeList, string name, string time)
+    {
+      TypeList = typeList;
+      Name = name;
+      Time = time;
+    }
+  }
+}
