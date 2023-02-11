beaff8d7beb36011c7363e0922f11c272ae20999
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-11 23:46:34 +0700

    090223_Homework1

---
+++ 090223_Homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _090223_Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
