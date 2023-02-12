ed03b1db51e9be99f4d2a80a13c78cbe6332caf7
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-12 23:11:01 +0700

    060223_Homework4

--- 060223_Homework1/IPerson.cs
+++ 060223_Homework1/IPerson.cs
@@ -3,7 +3,7 @@ namespace _060223_Homework1
   internal interface IPerson
   {
     //Auto implemented properties
-    string Name { get; set; }
-    int Age { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
   }
 }
