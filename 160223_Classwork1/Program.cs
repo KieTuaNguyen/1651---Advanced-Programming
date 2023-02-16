0c91c2e0d604cc2ee92db9a2e6bc420054b5629d
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-16 16:26:25 +0700

    160223_Classwork1

---
+++ 160223_Classwork1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _160223_Classwork1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+      //Normal way
+      School school1 = new School("Hogwarts", "1234 Hogwarts Way", "USA");
+      Console.WriteLine(school1);
+
+      School school2 = new School("Truong doi", "193 Nguyen Luong Bang", "Viet Nam");
+      Console.WriteLine(school2);
+      */
+
+      /*
+      //Singleton pattern
+      School school1 = School.Instance;
+      school1.Name = "Hogwarts";
+      school1.Address = "1234 Hogwarts Way";
+      school1.City = "USA";
+      Console.WriteLine(school1);
+
+      School school2 = School.Instance;
+      school2.Name = "Truong doi";
+      school2.Address = "193 Nguyen Luong Bang";
+      school2.City = "Viet Nam";
+      Console.WriteLine(school2);
+
+      Console.WriteLine(school1);
+
+      Console.WriteLine(school1.Equals(school2));
+      */
+    }
+  }
+}
---
+++ 160223_Classwork1/School.cs
@@ -0,0 +1,56 @@
+namespace _160223_Classwork1
+{
+  class School
+  {
+    /*
+    //Normal way
+    //auto implemented properties
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+
+    //constructor
+    public School(string name, string address, string city)
+    {
+      Name = name;
+      Address = address;
+      City = city;
+    }
+
+    //method
+    public override string ToString()
+    {
+      return Name + " " + Address + " " + City;
+    }
+    */
+
+    /**/
+    //Singleton pattern
+    private static School instance = null;
+    private School()
+    {
+      // Constructor is marked private to prevent external instantiation
+    }
+
+    public static School Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new School();
+        }
+        return instance;
+      }
+    }
+
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+
+    public override string ToString()
+    {
+      return Name + " " + Address + " " + City;
+    }
+  }
+}
